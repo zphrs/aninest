2dadc834c07e9ae69cdc26f80f04ea07f4f6b481
zphrs <46908127+zphyrj@users.noreply.github.com>
2024-03-08 06:11:08 -0800

    Updates

---
+++ assets/Vec2_index.md.DQgGCYdK.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,c as e,o as a,a4 as l}from"./chunks/framework.nQaBHiNx.js";const y=JSON.parse('{"title":"Vec2","description":"","frontmatter":{},"headers":[],"relativePath":"Vec2/index.md","filePath":"Vec2/index.md"}'),n={name:"Vec2/index.md"},r=l("",12),i=[r];function o(d,s,c,f,h,u){return a(),e("div",null,i)}const x=t(n,[["render",o]]);export{y as __pageData,x as default};
