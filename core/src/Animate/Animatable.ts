3abbfe57ec79530e3a03f1081dec571fda0e2dde
zphrs <z@zephiris.dev>
2025-06-18 23:31:49 -0700

    fixed up docs slightly

--- core/src/Animate/Animatable.ts
+++ core/src/Animate/Animatable.ts
@@ -202,7 +202,7 @@ export type ListenerSuppressor =
  * @param to The new partial state of the animation. A partial state
  * means that if the complete state is `{ a: 0, b: 0 }` and you call `modifyTo(anim, { a: 1 })`,
  * the new target state will be `{ a 1, b: 0 }`.
- * @param suppressStartListeners If true, the start listeners will not be called. Useful for
+ * @param suppressListeners If true, the start listeners will not be called. Useful for
  * when you want to modify the animation within a start listener without causing an infinite loop.
  * You can also pass an object with `start` and `interrupt` keys to selectively suppress listeners.
  * By default `true` suppresses the start listeners while `false` suppresses nothing. This means
