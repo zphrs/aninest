f9ce969fc7b870ff0d495a36cb693c98fce7949b
zphrs <z@zephiris.dev>
2026-07-01 17:08:59 -0700

    remove css from index for now

--- extensions/src/index.ts
+++ extensions/src/index.ts
@@ -1,10 +1,9 @@
-export * from "./loop"
-export * from "./snap"
-export * from "./bound"
-export * from "./cache"
-export * from "./dynamicDuration"
-export * from "./update"
-export * from "./proxy"
-export { localMomentumLayer } from "./momentum"
-export * from "./restrict"
-export * from "./css"
+export * from "./loop";
+export * from "./snap";
+export * from "./bound";
+export * from "./cache";
+export * from "./dynamicDuration";
+export * from "./update";
+export * from "./proxy";
+export { localMomentumLayer } from "./momentum";
+export * from "./restrict";
